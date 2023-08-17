2b558f184e50f138f33320cdec3b16188a837457
renzo-guarise <renzo.guarise@mercadolibre.com>
2023-08-16 22:06:12 -0300

    Haar cascade model

--- Emotion-detection/videotester.py
+++ Emotion-detection/videotester.py
@@ -25,32 +25,27 @@ while True:
         continue
     gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
-    if len(faces_detected)>0 :
-        top, right, bottom, left = faces_detected[0]
-        for (x, y, w, h) in faces_detected:
-            cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
-            roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
-            roi_gray = cv2.resize(roi_gray, (224, 224))
-            img_pixels = image.img_to_array(roi_gray)
-            img_pixels = np.expand_dims(img_pixels, axis=0)
-            img_pixels /= 255
+    top, right, bottom, left = faces_detected[0]
+    for (x, y, w, h) in faces_detected:
+        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+        roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
+        roi_gray = cv2.resize(roi_gray, (224, 224))
+        img_pixels = image.img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+        img_pixels /= 255
 
-            predictions = model.predict(img_pixels)
+        predictions = model.predict(img_pixels)
 
-            # find max indexed array
-            max_index = np.argmax(predictions[0])
+        # find max indexed array
+        max_index = np.argmax(predictions[0])
 
-            emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
-            predicted_emotion = emotions[max_index]
-            test_img[top:bottom, left:right]
-            cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+        predicted_emotion = emotions[max_index]
+        test_img[top:bottom, left:right]
+        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         resized_img = cv2.resize(test_img, (1000, 700))
-        cv2.imshow('Facial emotion analysis ', resized_img)
-        cv2.moveWindow("Facial emotion analysis ",top,left)
-
-    if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
-        break
+        
 
 cap.release()
 cv2.destroyAllWindows
---
+++ Haar_Cascade/model.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+f_cascade = cv2.CascadeClassifier("face.xml")
+e_cascade = cv2.CascadeClassifier("eye.xml")
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+    if type(frame) is not str:
+        image = cv2.imread(frame)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = f_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            eyes = e_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv2.imshow('img',image)
+
+    if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+        break
